4447acc72d8fbb81a8c89184580c594496a28b21
chanbormeymao <102600004@student.swin.edu.au>
2021-09-29 16:27:54 +1000

    Commit-Almed

--- Menu.php
+++ Menu.php
@@ -2,42 +2,42 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <title>Menu</title>
+  <meta charset="UTF-8">
+  <title>Menu</title>
 
-    <link href="menu.css" rel="stylesheet">
+  <link href="menu.css" rel="stylesheet">
 </head>
 
 <body>
-    <div class="box-area">
-         <center>
-        <header>
+  <div class="box-area">
+    <center>
+      <header>
 
-            <nav>
-              <div class="sale">
-                <a href="CreateSale.php">Create New Sale</a>
-                <a href="ReviewSale.php">View Sales</a>
-                <a href="ReturnedSale.php">Return Product</a>
-              </div>
-              <div class="prod">
-                <a href="AddProducts.php">Create New Product</a>
-                <a href="Product_View.php">View All Product</a>
-                <a href="editing_Product.php">Hide a product</a>
-              </div>
-                <a href="#">Stock Managment</a>
-                </nav>
-    </div>
-    </header>
+        <nav>
+          <div class="sale">
+            <a href="CreateSale.php">Create New Sale</a>
+            <a href="ReviewSale.php">View Sales</a>
+            <!-- <a href="ReturnedSale.php">Return Product</a> -->
+          </div>
+          <div class="prod">
+            <a href="AddProducts.php">Create New Product</a>
+            <a href="Product_View.php">View All Product</a>
+            <!-- <a href="editing_Product.php">Hide a product</a> -->
+          </div>
+          <a href="#">Stock Managment</a>
+        </nav>
+  </div>
+  </header>
   </center>
-    <div class="banner-area">
+  <div class="banner-area">
 
+  </div>
+  <div class="content-area">
+    <div class="wrapper">
+      <h2>About Us</h2>
+      <p>Information</p>
     </div>
-    <div class="content-area">
-        <div class="wrapper">
-            <h2>About Us</h2>
-            <p>Information</p>
-        </div>
-    </div>
+  </div>
 </body>
 
 </html>
