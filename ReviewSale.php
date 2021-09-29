0453859ee86cb2d978d008bf61146658aa2bc35a
chanbormeymao <102600004@student.swin.edu.au>
2021-09-29 16:58:19 +1000

    Commit-Almed

--- ReviewSale.php
+++ ReviewSale.php
@@ -1,271 +1,203 @@
-<?php
-include_once 'DBconnect.php';
-?>
-
-
-<!--search engine-->
-
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <title></title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="ReviewSale1.css">
+    <title>Review Sale</title>
+
 </head>
 
 <body>
-    <center>
-        <ul>
-            <li><a href="Menu.php">Menu</a></li>
+    <ul>
+        <li><a href="Menu.php">Menu</a></li>
 
-        </ul>
-        <form method="post" action="#">
-
-            <fieldset>
-                <legend>
-                    <h3>SEARCH ENGINE</h3>
-                </legend>
-                <p>NOTE: Enter the product name to search in our database</p>
-                <label for="input">Product name:</label>
-                <input type="text" name="input" id="input"><br><br>
-                <p> <input type="submit" value="Search" name="seachbar" /></p>
-            </fieldset>
-        </form>
-        <?php
-        //require_once("setting.php");//connected the database
-        //check if the connection is successful
-        if (isset($_POST["seachbar"])) {
-            //echo "successful";
-            //get data from HTML
-            $input = trim($_POST["input"]);
-            $input = htmlspecialchars($_POST["input"]);
-            //upon successful connect
-            //$sql_table="orders";
-            //set up the SQL command to query or adddata into the table
-            $query = "select * from product where product_name like '$input'";
-            //execute the query and store result into the result pointer
-            $result = mysqli_query($conn, $query);
-
-            if (!$result) {
-                echo "<p class=\"wrong\">Something is wrong with ", $query, "</p>";
-            }
-            if (mysqli_num_rows($result) == 0) {
-                echo "<div style='background-color:red;'><p>There is no data</p></div>";
-            } else {
-                echo "<p><b>This is the table </b></p>";
-                echo "<table border=\"1\">\n";
-                echo "<tr>\n" .
-                    "<th scope=\"col\">product_ID</th>\n" .
-                    "<th scope=\"col\">product_name</th>\n" .
-                    "<th scope=\"col\">product_desc</th>\n" .
-                    "<th scope=\"col\">Product_price</th>\n" .
-                    "<th scope=\"col\">images</th>\n" .
-                    "</tr>\n";
-
-                //retrieved current report pointed by the result pointer
-                // get all the info until it null
-                while ($row = mysqli_fetch_assoc($result)) {
-                    echo "<tr>\n";
-                    // align the text in the table cell
-                    echo "<td align=center>", $row["product_ID"], "</td>\n";
-                    echo "<td>", $row["product_name"], "</td>\n";
-                    echo "<td>", $row["product_desc"], "</td>\n";
-                    echo "<td align=center>", $row["Product_price"], "($)</td>\n";
-                    echo "<td>", "<img width=\"200\" src='images/" . $row['images'] . "' >", "</td>\n";
-                    echo "<td>";
-
-                    //Click to update
-                    echo "<a href='.php?product_ID=" . $row['product_ID'] . "' title='Update Record' data-toggle='tooltip'><span class='glyphicon glyphicon-pencil'></span></a>";
-                    /*  //Click to delete
-                                            echo "<a href='delete.php?product_ID=". $row['product_ID'] ."' title='Delete Record' data-toggle='tooltip'><span class='glyphicon glyphicon-trash'></span></a>";
-                                        echo "</td>";*/
-                    echo "</tr>\n";
-                }
-                echo "</table>\n";
-                mysqli_free_result($result);
-            } //if successful database connection
-            //close the database connection 
-            //mysqli_close($conn);	
-        }
-
-
-        ?>
-    </center>
-</body>
-
-</html>
-
-
-<!DOCTYPE html>
-<html>
-
-<head>
-    <center>
-        <meta charset="utf-8">
-
-
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-        <!--<link rel="stylesheet" href="Style/Addproducts.css">-->
-
-        <title></title>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.js"></script>
-
-        <script type="text/javascript">
-            $(document).ready(function() {
-                $('[data-toggle="tooltip"]').tooltip();
-            });
-        </script>
-        <style>
-            table,
-            th,
-            td {
-                border: 1px solid black;
-            }
-
-            body {
-                font-size: 2em;
-                line-height: 2em;
-            }
-        </style>
-        <style>
-            p.thick {
-                font-weight: bold;
-            }
-        </style>
-        <!--style the navigation bar-->
-        <style>
-            ul {
-                list-style-type: none;
-                margin: 0;
-                padding: 0;
-                overflow: hidden;
-                background-color: #333;
-            }
-
-            li {
-                float: left;
-            }
-
-            li a {
-                display: block;
-                color: white;
-                text-align: center;
-                padding: 14px 16px;
-                text-decoration: none;
-            }
-
-            li a:hover:not(.active) {
-                background-color: #111;
-            }
-
-            .active {
-                background-color: #04AA6D;
-            }
-
-            .edit_sign {
-                font-size: 3rem;
-            }
-
-            .delete_sign {
-                font-size: 3rem;
-            }
-        </style>
-        <!--End of styling-->
-
-        <!--Footer styling-->
-        <style>
-            .all-browsers {
-                margin: 0;
-                padding: 5px;
-                background-color: lightgray;
-            }
-
-            .all-browsers>h1,
-            .browser {
-                margin: 10px;
-                padding: 5px;
-            }
-
-            .browser {
-                background: white;
-            }
-
-            .browser>h2,
-            p {
-                margin: 4px;
-                font-size: 90%;
-            }
-
-            footer {
-                text-align: center;
-                padding: 3px;
-                background-color: #333;
-                color: white;
-            }
-        </style>
-        <!--end of footer styling-->
-</head>
-
-<body>
-
-    <h1>Product Table</h1>
+    </ul>
+    <form action="ReviewSale.php" method="POST" name="ReviewSale">
+        <div>
+            <span>
+                <input type="text" name="sale_ID" placeholder="Sale ID">
+            </span>
+            <span>
+                <button type="submit" name="SaleIDSearch" class="button">Search</button>
+            </span>
+        </div>
+        <div>
+            <span>
+                <input type="date" name="date" placeholder="Date">
+            </span>
+            <span>
+                <button type="submit" name="DateSearch" class="button">Search</button>
+            </span>
+        </div>
+    </form>
     <?php
-    $sql = "SELECT * FROM product;";
-    $result = mysqli_query($conn, $sql);
-    $resultCheck = mysqli_num_rows($result);
-    if (!$result) {
-        echo "<p class=\"wrong\">Something is wrong with ", $sql, "</p>";
-    }
-    if (mysqli_num_rows($result) == 0) {
-        echo "<div style='background-color:red;'><p>There is no data</p></div>";
-    } else {
-        echo "<table border=\"1\">\n";
-        echo "<tr>\n" .
-            "<th scope=\"col\">product_ID</th>\n" .
-            "<th scope=\"col\">product_name</th>\n" .
-            "<th scope=\"col\">product_desc</th>\n" .
-            "<th scope=\"col\">Product_price</th>\n" .
-            "<th scope=\"col\">images</th>\n" .
-
-            "</tr>\n";
-
-        //retrieved current report pointed by the result pointer
-        // get all the info until it null
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo "<tr>\n";
-            // align the text in the table cell
-            echo "<td align=center>", $row["product_ID"], "</td>\n";
-            echo "<td>", $row["product_name"], "</td>\n";
-            echo "<td>", $row["product_desc"], "</td>\n";
-            echo "<td align=center>", $row["Product_price"], "($)</td>\n";
-            echo "<td>", "<img width=\"200\" src='images/" . $row['images'] . "' >", "</td>\n";
-            echo "<td>";
-
-            //Click to update
-            echo "<a href='editing_Product.php?product_ID=" . $row['product_ID'] . "' title='Update Record' data-toggle='tooltip'><span class='glyphicon glyphicon-pencil edit_sign'></span></a>";
-            echo "<span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>";
-            //Click to delete
-            // echo "<a href='delete.php?product_ID=" . $row['product_ID'] . "' title='Delete Record' data-toggle='tooltip'><span class='glyphicon glyphicon-trash delete_sign'></span></a>";
-            echo "</td>";
-            echo "</tr>\n";
-        }
-        echo "</table>\n";
-        mysqli_free_result($result);
-    } //if successful database connection
-    //close the database connection 
-    mysqli_close($conn);
-
-
-
+    include("DBconnect.php");
+    session_start();
     ?>
-    <br>
-    <footer>
-        Copyright 2021<br>
-        Author:group 13<br>
-        Email:102579094@student.swin.edu.au
-    </footer>
-    </center>
+    <table style="width:100%;">
+        <tr>
+            <td>Sale ID</td>
+            <td>Date</td>
+            <td>Time</td>
+            <td>Product</td>
+            <td>Total</td>
+            <td>Edit</td>
+
+        </tr>
+        <?php
+        if (isset($_POST["SaleIDSearch"])) {
+            if (isset($_POST["sale_ID"])) {
+                include("DBconnect.php");
+                $sale_ID = trim($_POST["sale_ID"]);
+                $SESSION["sale_record_by_saleID"] = RetriveDataBySaleID($sale_ID, $conn);
+                $SESSION["Searched_Product_Info"] =  SaleDataPrep($SESSION["sale_record_by_saleID"]);
+                PrintDataBySaleID($SESSION["sale_record_by_saleID"],  $SESSION["Searched_Product_Info"]);
+            } else {
+                $product_error = "<p style=\"color:red;\">No Product Name Input</p>";
+            }
+        } else if (isset($_POST["DateSearch"])) {
+            if (isset($_POST["date"])) {
+                include("DBconnect.php");
+                $date = trim($_POST["date"]);
+                $date = date_create($date);
+                $date = date_format($date, "d-m-Y");
+                $saleID_dateSearch = RetriveDataByDate($date, $conn);
+
+                for ($i = 0; $i < count($saleID_dateSearch); $i++) {
+                    include("DBconnect.php");
+                    $allSaleUDate = RetriveDataBySaleID($saleID_dateSearch[$i], $conn);
+                    $allProductSaleUDate = SaleDataPrep($allSaleUDate);
+                    PrintDataBySaleID($allSaleUDate,  $allProductSaleUDate);
+                }
+            } else {
+                $product_error = "<p style=\"color:red;\">No Product Name Input</p>";
+            }
+        }
+        ?>
+
+
+    </table>
+    <div><button class=button><a href="Menu.php">Back to Menu</a></button></div>
+
+
+    <?php
+    function RetriveDataBySaleID($sale_ID, $conn)
+    {
+        $query = "SELECT sale_product.sale_ID, sale.sale_date, sale.sale_time,sale_product.product_ID ,product.product_name,sale_product.sale_PQuantity ,sale.sale_price,product.images  
+        FROM sale_product 
+        INNER JOIN sale
+        ON sale.sale_ID = sale_product.sale_ID
+        INNER JOIN product
+        ON sale_product.product_ID = product.product_ID
+        WHERE sale_product.sale_ID = $sale_ID";
+        $result = mysqli_query($conn, $query);
+        $sale_info = array();
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $sale_info_new[] = array(
+
+                    "sale_ID" => @$row["sale_ID"],
+                    "sale_date" => @$row["sale_date"],
+                    "sale_time" => @$row["sale_time"],
+                    "product_ID" => @$row["product_ID"],
+                    "product_name" => @$row["product_name"],
+                    "images" => @$row["images"],
+                    "qnty" => @$row["sale_PQuantity"],
+                    "total" => @$row["sale_price"]
+                );
+                $sale_info = $sale_info + $sale_info_new;
+            }
+        } else {
+            echo "0 results";
+            return null;
+        }
+        // print_r($sale_info);
+        $conn->close();
+        return $sale_info;
+    }
+
+    function SaleDataPrep($sale_info)
+    {
+        $product_ID_arr = array();
+        $temp = array();
+        for ($i = 0; $i < count($sale_info); $i++) {
+            for ($j = $i; $j < count($sale_info); $j++) {
+                if (in_array($sale_info[$j]["sale_ID"], $sale_info[$i])) {
+                    $temp = array(
+                        "images" => @$sale_info[$i]["images"],
+                        "product_ID" => @$sale_info[$i]["product_ID"],
+                        "product_name" => @$sale_info[$i]["product_name"],
+                        "product_Qty" => @$sale_info[$i]["qnty"]
+                    );
+                }
+            }
+            array_push($product_ID_arr, $temp);
+        }
+        return $product_ID_arr;
+    }
+    function PrintDataBySaleID($arrSale, $arrProduct)
+    {
+    ?>
+        <tr>
+            <td><?php echo $arrSale[0]["sale_ID"]; ?></td>
+            <td><?php echo $arrSale[0]["sale_date"]; ?></td>
+            <td><?php echo $arrSale[0]["sale_time"]; ?></td>
+            <td>
+                <table style="width:80%;">
+                    <tr>
+                        <td>Picture</td>
+                        <td>Name</td>
+                        <td>Quantity</td>
+                    </tr>
+                    <?php foreach ($arrProduct as $row) { ?>
+                        <tr>
+                            <td><img src="images/<?php echo $row["images"]; ?>" alt="Product Picture"></td>
+                            <td><?php echo $row["product_name"]; ?></td>
+                            <td><?php echo $row["product_Qty"]; ?></td>
+                        </tr>
+                    <?php } ?>
+                    <?php ?>
+                </table>
+            </td>
+            <td><?php echo $arrSale[0]["total"]; ?></td>
+            <td><button class="button"><a href="ReturnedSale.php?action1=edit&idEdit=<?php echo $arrSale[0]["sale_ID"]; ?>" value="Edit">Edit</a></button></td>
+        </tr>
+
+        <?php
+        // }
+        ?>
+    <?php
+    }
+    function RetriveDataByDate($date, $conn)
+    {
+        $query = "SELECT sale_ID FROM sale WHERE sale_date = '$date'";
+        $result = mysqli_query($conn, $query);
+        $Xa = array();
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $sale_id_new[] = array(
+                    "sale_ID" => @$row["sale_ID"]
+                );
+            }
+        } else {
+            echo "No Match";
+            return null;
+        }
+        foreach ($sale_id_new as $x) {
+            array_push($Xa, $x["sale_ID"]);
+        }
+        return ($Xa);
+    }
+    function RetriveDataByDateTime()
+    {
+        # code...
+    }
+    ?>
+
 </body>
 
 </html>
