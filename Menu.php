d2488f50bb2df825f1177b779a2f98dbba1b15ee
chanbormeymao <102600004@student.swin.edu.au>
2021-09-29 16:17:40 +1000

    Commit-Almed

--- Menu.php
+++ Menu.php
@@ -10,25 +10,25 @@
 
 <body>
     <div class="box-area">
-        <center>
-            <header>
+         <center>
+        <header>
 
-                <nav>
-                    <div class="sale">
-                        <a href="CreateSale.php">Create New Sale</a>
-                        <a href="ReviewSale.php">View Sales</a>
-                        <a href="ReturnedSale.php">Return Product</a>
-                    </div>
-                    <div class="prod">
-                        <a href="AddProducts.php">Create New Product</a>
-                        <a href="Product_View.php">View All Product</a>
-                        <a href="editing_Product.php">Hide a product</a>
-                    </div>
-                    <a href="#">Stock Managment</a>
+            <nav>
+              <div class="sale">
+                <a href="CreateSale.php">Create New Sale</a>
+                <a href="ReviewSale.php">View Sales</a>
+                <a href="ReturnedSale.php">Return Product</a>
+              </div>
+              <div class="prod">
+                <a href="AddProducts.php">Create New Product</a>
+                <a href="Product_View.php">View All Product</a>
+                <a href="editing_Product.php">Hide a product</a>
+              </div>
+                <a href="#">Stock Managment</a>
                 </nav>
     </div>
     </header>
-    </center>
+  </center>
     <div class="banner-area">
 
     </div>
