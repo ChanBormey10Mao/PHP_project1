e39bf94ebd68e9952cf8ffdff8abfc33050dcf74
chanbormeymao <102600004@student.swin.edu.au>
2021-09-29 16:26:01 +1000

    Commit-Almed

--- CreateSaleProcess.php
+++ CreateSaleProcess.php
@@ -11,10 +11,7 @@
 
 <body>
     <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#news">News</a></li>
-        <li><a href="#contact">Contact</a></li>
-        <li style="float:right"><a class="active" href="#about">About</a></li>
+        <li><a href="Menu.php">Home</a></li>
     </ul>
     <center>
         <?php
