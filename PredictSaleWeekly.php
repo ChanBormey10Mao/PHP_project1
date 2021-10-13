4cc420920655086c093c3c4670462c63ef52c719
chanbormeymao <102600004@student.swin.edu.au>
2021-10-13 17:08:51 +1100

    Commit-Mey- Weekly sale

--- PredictSaleWeekly.php
+++ PredictSaleWeekly.php
@@ -198,7 +198,7 @@
     }
     function RetrieveDataDB($conn)
     {
-        $query = "SELECT sale_product.product_ID, product.product_name, SUM(sale_product.sale_PQuantity) as sale_PQuantity,sale.sale_date,ROUND(sale_product.sale_PQuantity * product.Product_price,2) AS Price, 
+        $query = "SELECT sale_product.product_ID, product.product_name, SUM(sale_product.sale_PQuantity) as sale_PQuantity,sale.sale_date,ROUND(SUM(sale_product.sale_PQuantity) * product.Product_price,2) AS Price, 
         -- DATE_ADD(sale.sale_date, INTERVAL(-DAYOFWEEK(sale.sale_date)) DAY) as start_date,
         -- DATE_ADD(sale.sale_date, INTERVAL(-DAYOFWEEK(sale.sale_date)) DAY) as end_date, 
         WEEK(sale.sale_date,3) as Week_No 
