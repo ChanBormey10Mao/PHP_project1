b1498e104f7853b2d00edaccdd143c5072b3fa52
chanbormeymao <102600004@student.swin.edu.au>
2021-10-18 22:01:00 +1100

    Commit-Mey- Weekly sale

--- PredictSaleWeekly.php
+++ PredictSaleWeekly.php
@@ -120,8 +120,7 @@
             <p>The avergae sale price made from <?php echo $_SESSION["SalePredict"][0]["product_name"]; ?> is <span style="color:green;"><?php echo $_SESSION["price_avg_productID"]; ?></span> </p>
             <div>
                 <button><a href="PredictSaleWeekly.php">Back</a></button>
-                <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-                <button><a href="Menu.php">Menu</a></button>
+                <br>
             </div>
         </div>
         <div>
