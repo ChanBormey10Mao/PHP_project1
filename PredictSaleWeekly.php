079ef5d820b6e8d96889925db0ac57a18ace697a
AhmedElmi127 <a.elmi127@gmail.com>
2021-10-18 15:32:41 +1100

    PSWCSS

--- PredictSaleWeekly.php
+++ PredictSaleWeekly.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Predict Sale Weekly</title>
+    <link rel="stylesheet" href="PredictSaleWeekly.css">
 </head>
 
 <body>
@@ -71,7 +72,7 @@
         <input type="submit" name="submitSearch" value="View Prediction">
     </form>
 
-    <table style="width: 100%;">
+    <table style="width: 100%;" class = "content-table">
         <tr>
             <td>Product ID</td>
             <td>Product Name</td>
