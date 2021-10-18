6850a3979b9240a0068257e618e425d17e059679
Chingsien12 <87092668+Chingsien12@users.noreply.github.com>
2021-10-18 17:04:32 +1100

    nothing

--- ReviewSale.php
+++ ReviewSale.php
@@ -32,14 +32,16 @@
                 <button type="submit" name="DateSearch" class="button">Search</button>
             </span>
         </div>
-        <div>
-            sv  <a href="export.php">Export file to CSV</a>
+        
+    </form>
+    
+            <button><a href="export.php">Export file to CSV</a></button>
+
         <!-- <form method="post" action="export.php" align="center"> 
            
                      <input type="submit" name="export" value="CSV Export" class="btn btn-success" />  
         </form>  -->
-        </div>
-    </form>
+    
     <?php
     include("DBconnect.php");
     session_start();
