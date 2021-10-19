2dc0f5107372f23d135317e471a413262aba6abd
chanbormeymao <102600004@student.swin.edu.au>
2021-10-19 22:12:33 +1100

    Commit-Mey- Week  sale

--- login.php
+++ login.php
@@ -4,8 +4,8 @@ session_start();
 
 // Check if the user is already logged in, if yes then redirect him to index page
 if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
-    header("location: Menu.php");
-    exit;
+    // header("location: Menu.php");
+    // exit;
 }
 
 // include DBconnect file
