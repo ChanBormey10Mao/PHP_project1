8f8cb51315bab36b3e46e89bbbf5801dea7ac701
Richard <richardly78@gmail.com>
2021-10-20 15:42:53 +1100

    employee

---
+++ AdminMenu.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Menu</title>
+
+  <link href="menu.css" rel="stylesheet">
+</head>
+
+<body>
+  <div class="box-area">
+    <center>
+      <header>
+
+        <nav>
+          <div class="sale">
+            <a href="CreateSale.php" class="design">Create New Sale</a>
+            <a href="ReviewSale.php" class="design"> View Sales</a>
+            <a href="logout.php" class="design">Sign Out</a>
+            <!-- <a href="ReturnedSale.php">Return Product</a> -->
+          </div>
+          <div class="prod">
+            <a href="AddProducts.php"class="design">Create New Product</a>
+            <a href="Product_View.php"class="design">View All Product</a>
+            <!-- <a href="editing_Product.php">Hide a product</a> -->
+          </div>
+          <div>
+            <a href="PredictSaleWeekly.php"class="design">View Weekly Sale Average</a>
+          </div>
+          <div>
+            <a href="PredictSaleMonthly.php"class="design">View Monthly Sale Average</a>
+          </div>
+          <a href="stock_management.php"class="design">Stock Managment</a>
+        </nav>
+  </div>
+  </header>
+  </center>
+  <div class="banner-area">
+    <!-- hello1 -->
+  </div>
+  <div class="content-area">
+    <div class="wrapper">
+      <h2>About Us</h2>
+      <p>Information</p>
+    </div>
+  </div>
+</body>
+
+</html>
