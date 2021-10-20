8f7301483fdb6db4aa565fc558f0d77c965b4bbe
Deveyfree <dejopozza414@gmail.com>
2021-10-20 14:23:23 +1100

    CSS for csv

--- csv.php
+++ csv.php
@@ -12,6 +12,7 @@ $result=mysqli_query($conn,$query);
            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>  
            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>  
+           <link rel="stylesheet" href="csv.css"
       </head>  
       <body>  
            <br /><br />  
