933eef0d7b2a12c4717186ae0a8a049f48a2822c
AhmedElmi127 <a.elmi127@gmail.com>
2021-10-28 23:10:55 +1100

    PSWCSS

--- PredictSaleWeekly.php
+++ PredictSaleWeekly.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Predict Sale Weekly</title>
+    <title></title>
     <link rel="stylesheet" href="PredictSaleWeekly.css">
 </head>
 
@@ -13,7 +13,7 @@
     <navbar>
         <ul class="nav-list">
             <li class="nav-item">
-                <a href="AdminMenu.php">Menu</a>
+                
             </li>
 
                     </ul>
